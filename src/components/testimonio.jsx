b2ff9762c4dc8714181301a694616930ba63a44e
humbertoAG <humbertoag@MacBook-Air-de-humbertoAG.local>
2023-04-13 00:14:31 -0500

    first confuguration

---
+++ src/components/testimonio.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+function Testimonio (){
+  return (
+    <div className='container-testimonials'>
+      <img className='imagen-testimonio'
+        src={require('../images/attractive-1869761_640.jpeg')} 
+        alt='foto-ema'
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">humberto alcantara</p>
+        <p className="cargo-testimonio">ingeniero en sitemas</p>
+        <p className="texto-testimonio">Lorem ipsum, dolor sit amet 
+        consectetur adipisicing elit. Eligendi corporis eveniet omnis commodi 
+        saepe delectus dignissimos maiores ab nulla animi.</p>
+      </div>
+    </div>
+  )
+}
+
+
+export default Testimonio
